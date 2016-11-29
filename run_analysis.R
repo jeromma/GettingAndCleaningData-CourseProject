0e6d7045769607e9740d184b3e91e4b7eef4ac95
Mark Jerome <m_jerome@bellsouth.net>
2016-11-29 08:03:07 -0500

    Added CodeBook.md and began to add comments to run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,19 +1,42 @@
+# run_analysis.R
+#
+# This script is part of the course project for Getting and Cleaning Data.
+#
+# The purpose of this project is to demonstrate my ability to collect,
+# work with, and clean a data set. The goal is to prepare tidy data that
+# can be used for later analysis.
+#
+# This script assumes the subdirectory "UCI HAR Dataset" containing the
+# Samsung data is in the working directory.  The Samsung data can be
+# downloaded from
+# https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
+#
+# The following functions are used from package dplyr:
+#   select, group_by, summarize_each
+#
 library(dplyr)
+#
+# Switch to Samsung data subdirectory, read all necessary data, and switch
+# back to original working directory.
+#
+savewd <- getwd()
 setwd("UCI HAR Dataset")
 features <- read.table("features.txt")
+activity_labels <- read.table("activity_labels.txt")
 y_train <- read.table("train/y_train.txt")
-names(y_train) <- "activityKey"
 X_train <- read.table("train/X_train.txt")
-names(X_train) <- features[[2]]
 subject_train <- read.table("train/subject_train.txt")
+y_test <- read.table("test/y_test.txt")
+X_test <- read.table("test/X_test.txt")
+subject_test <- read.table("test/subject_test.txt")
+setwd(savewd)
+
+names(y_train) <- "activityKey"
+names(X_train) <- features[[2]]
 names(subject_train) <- "subject"
 wide_train <- cbind(cbind(y_train, subject_train), X_train)
-
-y_test <- read.table("test/y_test.txt")
 names(y_test) <- "activityKey"
-X_test <- read.table("test/X_test.txt")
 names(X_test) <- features[[2]]
-subject_test <- read.table("test/subject_test.txt")
 names(subject_test) <- "subject"
 wide_test <- cbind(cbind(y_test, subject_test), X_test)
 
@@ -24,7 +47,6 @@ stdFinder <- grepl("std()", names(allData), fixed=TRUE)
 myColumns <- c(1, 2, which(meanFinder | stdFinder))
 narrowData <- allData[myColumns]
 
-activity_labels <- read.table("activity_labels.txt")
 names(activity_labels) <- c("activityKey", "activity")
 
 activityData <- merge(activity_labels, narrowData, by="activityKey")
@@ -34,4 +56,5 @@ activityData <- select(activityData, -activityKey)
 groupData <- group_by(narrowData, activityKey, subject)
 tidyData <- summarize_each(groupData, funs(mean))
 
+write.table(tidyData, "tidyData.txt", row.name=FALSE)
 
